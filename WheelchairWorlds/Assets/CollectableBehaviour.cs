b5ea5201609913b8fadaac99b29c0ae177958743
eyesniper2 <eyesniper2@rayfall.net>
2016-03-19 12:35:22 -0600

    Collectables now disappear

--- WheelchairWorlds/Assets/CollectableBehaviour.cs
+++ WheelchairWorlds/Assets/CollectableBehaviour.cs
@@ -3,14 +3,25 @@ using System.Collections;
 
 public class CollectableBehaviour : MonoBehaviour {
 
+	public GameObject collectableObject;
+
+
 	// Use this for initialization
 	void Start () {
 		
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 	
+	}
+
+	private void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject.name == "PlayerController")
+		{
+			collectableObject.SetActive (false);
+		}
 	}
 }
