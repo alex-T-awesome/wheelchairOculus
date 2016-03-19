578855e0c22569d71da8de6283c6f3a6d77e13f5
Rishi Barnwal <barnwal.rishi@gmail.com>
2016-03-19 13:32:50 -0600

    HUD
    Added score HUD

---
+++ WheelchairWorlds/Assets/WheelChairCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelChairCollider : MonoBehaviour {
+
+	public TextMesh HeadsUpDisplay;
+	int score = 2;
+
+	void OnCollisionEnter(Collision hit) {
+		if (hit.gameObject.tag == "Pickup") {
+			score++;
+			HeadsUpDisplay.text = "" + score;
+		} else if (hit.gameObject.tag == "Avoid") {
+			score--;
+			HeadsUpDisplay.text = "" + score;
+		}
+	}
+}
