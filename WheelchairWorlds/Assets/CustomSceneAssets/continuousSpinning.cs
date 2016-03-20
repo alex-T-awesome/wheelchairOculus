689ed783f0dd8ec876443ab6b5bce5eed5bed1d4
nrossol <nrossol@gmail.com>
2016-03-20 10:59:57 -0600

    main level updated

---
+++ WheelchairWorlds/Assets/CustomSceneAssets/continuousSpinning.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class continuousSpinning : MonoBehaviour {
+
+    public float rotationSpeedDegreesPerSec = 60.0f;
+    public Vector3 axisOfRotation = Vector3.forward;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(axisOfRotation, rotationSpeedDegreesPerSec * Time.deltaTime);
+	}
+}
